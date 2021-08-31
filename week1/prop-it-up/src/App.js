d21392ece895fa1bf979cfdb47c447b0f493b16f
Kunlun Chen <kollinchen1@hotmail.com>
2021-08-31 11:13:00 -0700

    big inversion

--- week1/prop-it-up/src/App.js
+++ week1/prop-it-up/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import PersonCard from './components/PersonCard';
+import BigInversion from './components/BigInversion';
 
 var peopleArr =[
   {"firstName":"Jane", "lastName":"Doe", "age":45, "hairColor":"Black"},
@@ -12,8 +13,15 @@ var peopleArr =[
 function App() {
   return (
     <div className="App">
+        <h1 style={{textDecoration: "underline"}}>Prop it Up</h1>
         {peopleArr.map(person => {
           return <PersonCard header={person.lastName +", " + person.firstName} firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor} />
+        })
+        
+        }
+        <h1 style={{textDecoration: "underline"}}>Big Inversion</h1>
+        {peopleArr.map(person => {
+          return <BigInversion header={person.lastName +", " + person.firstName} firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor} />
         })
         
         }
