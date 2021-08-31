0b9fcfda2e9201f8ed9dadbcc27b0047b78987c7
Kunlun Chen <kollinchen1@hotmail.com>
2021-08-30 22:43:08 -0700

    first jsx complete

--- week1/first-jsx/src/App.js
+++ week1/first-jsx/src/App.js
@@ -3,21 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>
+    <div style={{backgroundImage:`url(${logo})`}}>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      
+      <ul>
+        <li>Learn React</li>
+        <li>Skip Leg Day</li>
+        <li>Climb My. Everest</li>
+        <li>Feed the dogs</li>
+      </ul>
+      
+      
     </div>
   );
 }
